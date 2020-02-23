de21e502ef86c0a0884f291dc8100f4b9e265f9c
Hamish McNeish <hamish.mcneish@gmail.com>
2020-02-23 10:38:57 +1000

    removed api

--- app/requests/power.py
+++ app/requests/power.py
@@ -5,16 +5,6 @@ from app.db import redis
 from app.utils import json
 
 @crossdomain(origin='*')
-@app.route("/power/production")
-@returns_json
-def production():
-    result = redis.get_state(redis.REDIS_PRODUCTION_DATA, False)
-    if not result:
-        return json.dumps({'records': 0})
-    else:
-        return result
-
-@crossdomain(origin='*')
 @app.route("/power/inverter")
 @returns_json
 def inverter():
